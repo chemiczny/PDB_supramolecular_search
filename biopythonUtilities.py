4f4efa8b9b01bc1e8e39effe56b9afb455f600f5
chemiczny <glanowskimichal@gmail.com>
2018-06-29 14:13:31 +0200

    Add new file: biopython utilietes

---
+++ biopythonUtilities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 29 12:53:24 2018
+
+@author: michal
+"""
+
+def createResId(residue):
+    idNo = residue.get_id()[1]
+    chain = residue.get_parent().get_id()
+    name = residue.get_resname()
+    firstCoord = list(residue.get_atoms())[0].get_coord()
+    coordStr = str(firstCoord[0])+str(firstCoord[1])+str(firstCoord[2])
+    
+    return chain+str(idNo)+name+coordStr
+
+def createResIdFromAtom(atom):
+    return createResId( atom.get_parent() )
