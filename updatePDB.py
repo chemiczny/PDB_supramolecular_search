febcfb3ece47fd65b5b711893646bc000429478c
Michal Glanowski <plgglanow@login01.pro.cyfronet.pl>
2019-01-05 12:51:05 +0100

    Add script for update cif directory

---
+++ updatePDB.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, "/net/archive/groups/plggsuprm/pythonPackages/" )
+
+from Bio.PDB import PDBList
+import json
+from os.path import dirname
+
+configurationFileName = "config.json"
+configFile = open(configurationFileName)
+config = json.load(configFile)
+configFile.close()
+
+pdb_dir = dirname(config["cif"])
+pl = PDBList(pdb=pdb_dir)
+pl.flat_tree = True
+pl.update_pdb(file_format = "mmCif")
