4ef23d0eb96ca02c28cc28b5616350913df12d41
chemiczny <glanowskimichal@gmail.com>
2019-01-06 16:29:59 +0100

    Check progress in brutalFinish.py

--- brutalFinish.py
+++ brutalFinish.py
@@ -27,6 +27,23 @@ def mergeLogs( logFinal, logs  ):
         remove(log_file)
     
     final_log.close()
+    
+def mergeProgressFiles():
+    cifProcessed = 0
+    log_files = glob.glob("logs/partialProgress*")
+    for log_file in log_files:
+        log = open(log_file, 'r')
+        cifProcessed += int(log.readline())
+        log.close()
+        remove(log_file)
+    
+    cifNoFile = open("logs/cif2process.log", 'r')
+    cifNo = int(cifNoFile.readline())
+    cifNoFile.close()
+    
+    progressSummary = open("logs/progressSummary.log", 'w')
+    progressSummary.write("Przetworzono: " +str( cifProcessed ) + "/"+  str(cifNo)+"\n")
+    progressSummary.close()
 #
 mergeLogs("logs/anionPi.log", "logs/anionPi*.log"  )
 mergeLogs("logs/cationPi.log", "logs/cationPi*.log"  )
@@ -34,6 +51,8 @@ mergeLogs("logs/piPi.log", "logs/piPi*.log"  )
 mergeLogs("logs/anionCation.log", "logs/anionCation*.log"  )
 mergeLogs("logs/hBonds.log", "logs/hBonds*.log"  )
 mergeLogs("logs/additionalInfo.log", "logs/additionalInfo*.log"  )
+
+mergeProgressFiles()
 
 timeStop = time.time()
 timeFile = open("logs/timeStop.log", 'w')
