4b942455fc7b19e009303ea7b3e9fdd4b0d45934
chemiczny <glanowskimichal@gmail.com>
2019-03-14 23:04:18 +0100

    QMGUI almost fully operable

--- pymol_plugin/supramolecular/QMGUI.py
+++ pymol_plugin/supramolecular/QMGUI.py
@@ -6,6 +6,7 @@ Created on Mon Feb 25 16:21:16 2019
 @author: michal
 """
 import sys
+import os
 
 if sys.version_info[0] < 3:
     import Tkinter
@@ -37,12 +38,12 @@ class QMGUI:
         seleLabel = Tkinter.Label(self.page, text = "sele")
         seleLabel.grid(row=3, column=0)
         
-        seleAroundLabel = Tkinter.Label(self.page, text = "around sele")
-        seleAroundLabel.grid(row=4, column=0)
-        
-        self.seleRadius = Tkinter.Entry(self.page, width=5)
-        self.seleRadius.grid(row=5, column=0)
-        self.seleRadius.insert('end', "5")
+#        seleAroundLabel = Tkinter.Label(self.page, text = "around sele")
+#        seleAroundLabel.grid(row=4, column=0)
+#        
+#        self.seleRadius = Tkinter.Entry(self.page, width=5)
+#        self.seleRadius.grid(row=5, column=0)
+#        self.seleRadius.insert('end', "5")
         
         
     def gridHost(self):
@@ -59,13 +60,14 @@ class QMGUI:
         self.hostSele = Tkinter.Button(self.page, width = 5, text = "get", command = self.readHost)
         self.hostSele.grid(row=3, column =1)
         
-        self.hostAroundSele = Tkinter.Button(self.page, width = 5, text = "get", command = self.readHostFromSeleAround)
-        self.hostAroundSele.grid(row=4, column = 1)
+#        self.hostAroundSele = Tkinter.Button(self.page, width = 5, text = "get", command = self.readHostFromSeleAround)
+#        self.hostAroundSele.grid(row=4, column = 1)
         
     def readHost(self):
         try:
             stateNo = cmd.get_state()
             cmd.create("host", "sele", stateNo)
+            cmd.select( "hostFrozen", "none")
         except:
             print("lo kurla")
             
@@ -91,13 +93,14 @@ class QMGUI:
         self.guestSele = Tkinter.Button(self.page, text = "get" ,  width = 5, command = self.readGuest)
         self.guestSele.grid(row=3, column =2)
         
-        self.guestAround = Tkinter.Button(self.page, text = "get", width = 5, command = self.readGuestFromSeleAround)
-        self.guestAround.grid(row=4, column=2)
+#        self.guestAround = Tkinter.Button(self.page, text = "get", width = 5, command = self.readGuestFromSeleAround)
+#        self.guestAround.grid(row=4, column=2)
         
     def readGuest(self):
         try:
             stateNo = cmd.get_state()
             cmd.create("guest", "sele", stateNo)
+            cmd.select("guestFrozen", "none")
         except:
             print("lo kurla")
             
@@ -123,11 +126,141 @@ class QMGUI:
         
     
     def gridWrite(self):
-        writeButton = Tkinter.Button(self.page, text = "write")
+        writeButton = Tkinter.Button(self.page, text = "write", command = self.write)
+        writeButton.grid(row=10, column = 0)
         
+    def write(self):
+        options = { "mustexist" : False , "title" : "Job directory selection"}
+        directory = tkFileDialog.askdirectory(**options)
+        if not directory:
+            return
+        
+        if not os.path.isdir(directory):
+            os.makedirs(directory)
+            
+        basename = os.path.basename(directory)
+        
+        slurmFile = os.path.join( directory, basename + ".slurm" )
+        inpFile = os.path.join(directory, basename + ".inp")
+        inpFileBasename = os.path.basename(inpFile)
+        
+        slurmF = open(slurmFile, 'w')
+        
+        slurmHead = self.slurmTextG16.get("1.0", "end")
+        
+        slurmF.write(slurmHead)
+        slurmF.write("\n")
+        
+        slurmF.write("\nmodule add plgrid/apps/gaussian/g16.A.03\n\n")
+        slurmF.write("g16 "+inpFileBasename+ "\n\n")
+        
+        slurmF.close()
+        
+        inpF = open(inpFile, 'w')
+        
+        inpF.write("%Chk="+inpFileBasename.replace(".inp",".chk")+"\n")
+        routeSection = self.routeSection.get("1.0", "end")
+        inpF.write(routeSection)
+        
+        inpF.write("\nEmilka jest najpiekniejsza!\n\n")
+        
+        chargeTotal = self.complexCharge.get()
+        spinTotal = self.complexSpin.get()
+        chargeGuest = self.guestCharge.get()
+        spinGuest = self.guestSpin.get()
+        chargeHost = self.hostCharge.get()
+        spinHost = self.hostSpin.get()
+        
+        inpF.write(chargeTotal+","+spinTotal+" "+chargeGuest+","+spinGuest+" "+chargeHost+","+spinHost+"\n")
+        model = cmd.get_model("guest and guestFrozen")
+        
+        for atom in model.atom:
+            resnum = atom.resi
+            resname = atom.resn
+            pdbname = atom.name
+            element = atom.symbol
+            
+            x = str(atom.coord[0])
+            y = str(atom.coord[1])
+            z = str(atom.coord[2])
+            
+            inpF.write(element+"(Fragment=1, PDBName="+pdbname+", ResName="+resname+", ResNum="+str(resnum)+") -1 "+x+" "+y+" "+z+"\n" )
+            
+            
+        model = cmd.get_model("guest and not guestFrozen")
+        
+        for atom in model.atom:
+            resnum = atom.resi
+            resname = atom.resn
+            pdbname = atom.name
+            element = atom.symbol
+            
+            x = str(atom.coord[0])
+            y = str(atom.coord[1])
+            z = str(atom.coord[2])
+            
+            inpF.write(element+"(Fragment=1, PDBName="+pdbname+", ResName="+resname+", ResNum="+str(resnum)+") "+x+" "+y+" "+z+"\n" )
+            
+            
+        model = cmd.get_model("host and hostFrozen")
+        
+        for atom in model.atom:
+            resnum = atom.resi
+            resname = atom.resn
+            pdbname = atom.name
+            element = atom.symbol
+            
+            x = str(atom.coord[0])
+            y = str(atom.coord[1])
+            z = str(atom.coord[2])
+            
+            inpF.write(element+"(Fragment=2, PDBName="+pdbname+", ResName="+resname+", ResNum="+str(resnum)+") -1 "+x+" "+y+" "+z+"\n" )
+            
+        model = cmd.get_model("host and not hostFrozen")
+        
+        for atom in model.atom:
+            resnum = atom.resi
+            resname = atom.resn
+            pdbname = atom.name
+            element = atom.symbol
+            
+            x = str(atom.coord[0])
+            y = str(atom.coord[1])
+            z = str(atom.coord[2])
+            
+            inpF.write(element+"(Fragment=2, PDBName="+pdbname+", ResName="+resname+", ResNum="+str(resnum)+") "+x+" "+y+" "+z+"\n" )
+            
+        inpF.write("\nstoichiometry=H2O1\n")
+        inpF.write("solventname=Water2\n")
+        inpF.write("eps=4\n")
+        inpF.write("epsinf=1.77556\n")
+        inpF.write("\n\n")
+        
+        inpF.close()
+        
+    def gridGaussianRouteSection(self):
+        routeSectionLabel = Tkinter.Label(self.page, text = "Gaussian route section")
+        routeSectionLabel.grid(row = 1, column=5, columnspan = 5)
+        
+        self.routeSection =Tkinter.Text(self.page, width =50, height = 10 )
+        self.routeSection.grid(row = 2, column = 5, columnspan = 5, rowspan = 5)
+        self.routeSection.insert("end", "%Mem=100GB\n#P B3LYP/6-31G(d,p)\n# Opt Counterpoise=2\n# SCRF(Solvent=Generic, Read)\n# Gfinput IOP(6/7=3)  Pop=full  Density  Test \n# Units(Ang,Deg)")
+        
+    def gridSlurmSection(self):
+        slurmSection = Tkinter.Label(self.page, text = "Slurm input")
+        slurmSection.grid(row = 1, column = 10, columnspan = 10)
+        
+        self.slurmTextG16 = Tkinter.Text(self.page, width = 50, height = 10)
+        self.slurmTextG16.grid(row = 2, column = 10 , columnspan = 5, rowspan = 5)
+        self.slurmTextG16.insert("end" , "#!/bin/env bash\n#SBATCH --nodes=1\n#SBATCH --cpus-per-task=24\n#SBATCH --time=70:00:00\n##### Nazwa kolejki\n#SBATCH -p plgrid\n" )
+                  
     
     def grid(self):
         self.gridBasicLabels()
         self.gridHost()
         self.gridGuest()
         self.gridComplex()
+        self.gridWrite()
+        self.gridSlurmSection()
+        
+        self.gridGaussianRouteSection()
