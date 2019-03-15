00677cbe5a86f393ac5f5fad86915eba8fa5dd4b
chemiczny <glanowskimichal@gmail.com>
2019-03-15 18:49:22 +0100

    New features in QMGUI: freezing atoms, choping aminoacid chains

--- pymol_plugin/supramolecular/QMGUI.py
+++ pymol_plugin/supramolecular/QMGUI.py
@@ -38,13 +38,46 @@ class QMGUI:
         seleLabel = Tkinter.Label(self.page, text = "sele")
         seleLabel.grid(row=3, column=0)
         
+        frozenLabel = Tkinter.Label(self.page, text = "frozen")
+        frozenLabel.grid(row=4, column=0, rowspan=2)
+        
+        addAtomsLabel = Tkinter.Label(self.page, text = "Add atoms")
+        addAtomsLabel.grid(row=6, column=0, rowspan=2)
+        
+        self.countAtoms = Tkinter.Button(self.page, width=5, text = "count at", command = self.countAtoms)
+        self.countAtoms.grid(row=8, column=0)
+        
 #        seleAroundLabel = Tkinter.Label(self.page, text = "around sele")
 #        seleAroundLabel.grid(row=4, column=0)
 #        
 #        self.seleRadius = Tkinter.Entry(self.page, width=5)
 #        self.seleRadius.grid(row=5, column=0)
 #        self.seleRadius.insert('end', "5")
+    def countAtoms(self):
+        try:
+            modelHost = cmd.get_model("host")
+            hostAtomNo =  len(modelHost.atom)
+        except:
+            hostAtomNo = 0
         
+        try:
+            modelGuest = cmd.get_model("guest")
+            guestAtomNo = len(modelGuest.atom)
+        except:
+            guestAtomNo = 0
+        
+        complexAtomNo = hostAtomNo + guestAtomNo
+        
+        self.guestAtomsNo.delete(0, "end")
+        self.guestAtomsNo.insert("end", str(guestAtomNo))
+        
+        self.hostAtomsNo.delete(0, "end")
+        self.hostAtomsNo.insert("end", str(hostAtomNo))
+        
+        self.complexAtomsNo.delete(0, "end")
+        self.complexAtomsNo.insert("end", str(complexAtomNo))
+
+
         
     def gridHost(self):
         hostLabel = Tkinter.Label(self.page, text = "Host")
@@ -60,8 +93,78 @@ class QMGUI:
         self.hostSele = Tkinter.Button(self.page, width = 5, text = "get", command = self.readHost)
         self.hostSele.grid(row=3, column =1)
         
+        self.hostFrozenClear = Tkinter.Button(self.page, width =5, text = "clear", command = self.clearHostFrozen)
+        self.hostFrozenClear.grid(row=4, column=1)
+        
+        self.hostFrozenDefault = Tkinter.Button(self.page, width = 5, text = "default", command = self.defaultHostFrozen)
+        self.hostFrozenDefault.grid(row=5, column=1)
+        
+        self.hostAddH = Tkinter.Button(self.page, width = 5, text = "addH", command = self.addHHost)
+        self.hostAddH.grid(row=6, column=1)
+        
+        self.hostAddNCA = Tkinter.Button(self.page, width = 5, text = "chop", command = self.hostChop)
+        self.hostAddNCA.grid(row=7, column=1)
+        
+        self.hostAtomsNo = Tkinter.Entry(self.page, width = 5)
+        self.hostAtomsNo.grid(row=8, column=1)
+        
 #        self.hostAroundSele = Tkinter.Button(self.page, width = 5, text = "get", command = self.readHostFromSeleAround)
 #        self.hostAroundSele.grid(row=4, column = 1)
+        
+    def clearHostFrozen(self):
+        cmd.select( "hostFrozen", "none")
+        
+    def defaultHostFrozen(self):
+        cmd.select("hostFrozen", " %hostFrozen or ( host and ( name CA ) ) ")
+        
+    def addHHost(self):
+        cmd.h_add("host")
+        
+    def hostChop(self):
+        try:
+            model = cmd.get_model("host")
+        except:
+            return
+        
+        chain2resId2atomNames = {}
+        
+        for atom in model.atom:
+            resnum = int(atom.resi)
+            pdbname = atom.name
+            chain = atom.chain
+            
+            if not chain in chain2resId2atomNames:
+                chain2resId2atomNames[chain] = { resnum : set([ pdbname ]) }
+            else:
+                if resnum in chain2resId2atomNames[chain]:
+                    chain2resId2atomNames[chain][resnum].add(pdbname)
+                else:
+                    chain2resId2atomNames[chain][resnum] = set([ pdbname ])
+                
+        for chain in chain2resId2atomNames:
+            resId2atomNames = chain2resId2atomNames[chain]
+            
+            resIds2addN = set([])
+            resIds2addC = set([])
+            
+            for resnum in resId2atomNames:
+                if resId2atomNames[resnum] != set( [ "CA", "C", "O" ]) and resId2atomNames[resnum] != set( [ "CA", "N" ]):
+                    resIds2addC.add(resnum+1)
+                    resIds2addN.add(resnum-1)
+                    
+            resIds2addN-= set(resId2atomNames.keys())
+            resIds2addC-= set(resId2atomNames.keys())
+            
+            stateNo = cmd.get_state()
+            for resnum in resIds2addC:
+                cmd.create("host", " %host or ( ( resi "+str(resnum)+ " and chain "+chain+ " ) and ( name CA or name N) ) ", stateNo)
+                cmd.bond( "%host and name N and resi "+str(resnum), "%host and name C and resi "+str(resnum-1) )
+                
+            for resnum in resIds2addN:
+                cmd.create("host", " %host or ( ( resi "+str(resnum)+ " and chain "+chain+ " ) and ( name CA or name C or name O) ) ", stateNo)
+                cmd.bond( "%host and name C and resi "+str(resnum), "%host and name N and resi "+str(resnum+1) )
+            
+        cmd.show("sticks", "host")
         
     def readHost(self):
         try:
@@ -93,8 +196,78 @@ class QMGUI:
         self.guestSele = Tkinter.Button(self.page, text = "get" ,  width = 5, command = self.readGuest)
         self.guestSele.grid(row=3, column =2)
         
+        self.guestFrozenClear = Tkinter.Button(self.page, width = 5, text = "clear", command = self.clearGuestFrozen)
+        self.guestFrozenClear.grid(row=4, column=2)
+        
+        self.guestFrozenDefault = Tkinter.Button(self.page, width = 5, text = "default", command = self.defaultGuestFrozen)
+        self.guestFrozenDefault.grid(row=5, column=2)
+        
+        self.guestAddH = Tkinter.Button(self.page, width = 5, text = "addH", command = self.addHGuest)
+        self.guestAddH.grid(row=6, column=2)
+        
+        self.guestAddNCA = Tkinter.Button(self.page, width = 5, text = "chop", command = self.guestChop)
+        self.guestAddNCA.grid(row=7, column=2)
+        
+        self.guestAtomsNo = Tkinter.Entry(self.page, width = 5)
+        self.guestAtomsNo.grid(row=8, column=2)
+        
 #        self.guestAround = Tkinter.Button(self.page, text = "get", width = 5, command = self.readGuestFromSeleAround)
 #        self.guestAround.grid(row=4, column=2)
+        
+    def guestChop(self):
+        try:
+            model = cmd.get_model("guest")
+        except:
+            return
+        
+        chain2resId2atomNames = {}
+        
+        for atom in model.atom:
+            resnum = int(atom.resi)
+            pdbname = atom.name
+            chain = atom.chain
+            
+            if not chain in chain2resId2atomNames:
+                chain2resId2atomNames[chain] = { resnum : set([ pdbname ]) }
+            else:
+                if resnum in chain2resId2atomNames[chain]:
+                    chain2resId2atomNames[chain][resnum].add(pdbname)
+                else:
+                    chain2resId2atomNames[chain][resnum] = set([ pdbname ])
+                
+        for chain in chain2resId2atomNames:
+            resId2atomNames = chain2resId2atomNames[chain]
+            
+            resIds2addN = set([])
+            resIds2addC = set([])
+            
+            for resnum in resId2atomNames:
+                if resId2atomNames[resnum] != set( [ "CA", "C", "O" ]) and resId2atomNames[resnum] != set( [ "CA", "N" ]):
+                    resIds2addC.add(resnum+1)
+                    resIds2addN.add(resnum-1)
+                    
+            resIds2addN-= set(resId2atomNames.keys())
+            resIds2addC-= set(resId2atomNames.keys())
+            
+            stateNo = cmd.get_state()
+            for resnum in resIds2addC:
+                cmd.create("guest", " %guest or ( ( resi "+str(resnum)+ " and chain "+chain+ " ) and ( name CA or name N) ) ", stateNo)
+                cmd.bond( "%guest and name N and resi "+str(resnum), "%guest and name C and resi "+str(resnum-1) )
+                
+            for resnum in resIds2addN:
+                cmd.create("guest", " %guest or ( ( resi "+str(resnum)+ " and chain "+chain+ " ) and ( name CA or name C or name O) ) ", stateNo)
+                cmd.bond( "%guest and name C and resi "+str(resnum), "%guest and name N and resi "+str(resnum+1) )
+            
+        cmd.show("sticks", "guest")
+        
+    def clearGuestFrozen(self):
+        cmd.select( "guestFrozen", "none")
+        
+    def defaultGuestFrozen(self):
+        cmd.select("guestFrozen", " %guestFrozen or ( guest and ( name CA  ) ) ")
+        
+    def addHGuest(self):
+        cmd.h_add("guest")
         
     def readGuest(self):
         try:
@@ -124,6 +297,9 @@ class QMGUI:
         self.complexSpin.grid(row=2, column =3)
         self.complexSpin.insert('end', '1')
         
+        self.complexAtomsNo = Tkinter.Entry(self.page, width = 5)
+        self.complexAtomsNo.grid(row=8, column=3)
+        
     
     def gridWrite(self):
         writeButton = Tkinter.Button(self.page, text = "write", command = self.write)
@@ -139,6 +315,17 @@ class QMGUI:
             os.makedirs(directory)
             
         basename = os.path.basename(directory)
+        
+        cmd.save( os.path.join(directory, basename+".pdb" ), "host or guest"  )
+        
+        additional = open( os.path.join(directory, basename+".dat"), 'w' )
+        objects = cmd.get_object_list()
+        for obj in objects:
+            if obj != "host" and obj != "guest":
+                additional.write(obj+"\n")
+        
+        
+        additional.close()
         
         slurmFile = os.path.join( directory, basename + ".slurm" )
         inpFile = os.path.join(directory, basename + ".inp")
@@ -230,10 +417,7 @@ class QMGUI:
             
             inpF.write(element+"(Fragment=2, PDBName="+pdbname+", ResName="+resname+", ResNum="+str(resnum)+") "+x+" "+y+" "+z+"\n" )
             
-        inpF.write("\nstoichiometry=H2O1\n")
-        inpF.write("solventname=Water2\n")
-        inpF.write("eps=4\n")
-        inpF.write("epsinf=1.77556\n")
+        inpF.write("\neps=4\n")
         inpF.write("\n\n")
         
         inpF.close()
@@ -244,7 +428,7 @@ class QMGUI:
         
         self.routeSection =Tkinter.Text(self.page, width =50, height = 10 )
         self.routeSection.grid(row = 2, column = 5, columnspan = 5, rowspan = 5)
-        self.routeSection.insert("end", "%Mem=100GB\n#P B3LYP/6-31G(d,p)\n# Opt Counterpoise=2\n# SCRF(Solvent=Generic, Read)\n# Gfinput IOP(6/7=3)  Pop=full  Density  Test \n# Units(Ang,Deg)")
+        self.routeSection.insert("end", "%Mem=100GB\n#P B3LYP/6-31G(d,p)\n# Opt Counterpoise=2\n# SCRF(Solvent=Water, Read)\n# Gfinput IOP(6/7=3)  Pop=full  Density  Test \n# Units(Ang,Deg)")
         
     def gridSlurmSection(self):
         slurmSection = Tkinter.Label(self.page, text = "Slurm input")
