b5b9198866837ce503958e70dd02d9cfaaa47da1
chemiczny <plgglanow@login01.pro.cyfronet.pl>
2020-04-06 00:54:59 +0200

    Keep only one script for downloading and updating the PDB

--- downloadPDB.py
+++
@@ -1,9 +0,0 @@
-from configure import configure
-config = configure()
-from Bio.PDB import PDBList
-from os.path import dirname
-
-pdb_dir = dirname(config["cif"])
-pl = PDBList(pdb=pdb_dir)
-pl.flat_tree = True
-pl.download_entire_pdb(file_format = "mmCif")
